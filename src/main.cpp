c461ef4a0719d2ce6ed1132199a881d216758ffa
GitHubAvailable <lhzmark@163.com>
2024-04-28 17:51:40 -0400

    docs: include buffer header

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 // Removing the line below may cause `SPI.h` missing error
 #include <Adafruit_CircuitPlayground.h> 
+#include "buffer.h"
 #include "setup.h"
 
 void setup() 
 {
     // put your setup code here, to run once:
+    Serial.begin(9600);
 }
 
 void loop() 
