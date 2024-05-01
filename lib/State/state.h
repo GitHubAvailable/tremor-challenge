9769b34ea5699cac5f6ff2cc83fefe0cb0d5bef0
GitHubAvailable <lhzmark@163.com>
2024-05-01 11:35:07 -0400

    docs: add available states enum

---
+++ lib/State/state.h
@@ -0,0 +1,24 @@
+/**
+ * states.h - Library of available states for the Parkinsonian 
+ * Tremor Detector.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the MIT-0 License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * MIT-0 License for more details.
+ * 
+ * Modified 1 May 2024 by Mark Liu
+ */
+#ifndef State_h
+#define State_h
+
+enum State
+{
+    Idle,
+    Measuring
+};
+
+#endif
